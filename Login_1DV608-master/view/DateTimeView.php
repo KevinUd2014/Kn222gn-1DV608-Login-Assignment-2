eb5272d5d7008054b1477b315c9b4250d3ae2acb
kn222gn <kn222gn@student.lnu.se>
2015-09-08 16:20:32 +0200

    Nu har jag gjort lite med klockan och jag hittade flera lösningar på den men vet inte vilken som är mest optimerad! ska fråga på handledning!

--- Login_1DV608-master/view/DateTimeView.php
+++ Login_1DV608-master/view/DateTimeView.php
@@ -3,10 +3,25 @@
 class DateTimeView {
 
 
+	
+
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		//$today = getdate();
+		//$time = gettimeofday();
+		
+		date_default_timezone_set("Europe/Stockholm");
 
+		//$day = date("jS");
+		//$date = date("H:m:s")
+		
+		//kan vara så att detta blir servertiden! och isåfall så kan tiden bli fel!
+		//måste ha med st, nd och th efter dagen!
+
+		//$timeString = "$today[weekday], the $today[mday] of $today[month] $today[year], The time is $today[hours]:$today[minutes]:$today[seconds]"; //kan ändra Hours osv till bara $date!!
+		
+		$timeString = date('l\, \t\h\e jS \o\f F Y\, \T\h\e \t\i\m\e \i\s H:i:s');//http://www.w3schools.com/php/func_date_date.asp
+																				  //http://php.net/manual/en/function.date.php
 		return '<p>' . $timeString . '</p>';
 	}
 }
