132bcd6632adf04eb5302cd71e81c80acc3ae48a
kevin <k3vin4real@gmail.com>
2015-09-09 18:35:37 +0200

    nu har jag lagt till 2 nya funktioner vet ej om det är rätt men tror det :) har också funderat på om jag ska ha ifsatser eller inte i den ena!

--- Login_1DV608-master/model/LoginModelNew.php
+++ Login_1DV608-master/model/LoginModelNew.php
@@ -4,10 +4,28 @@
 		//fick döpa den till new eftersom jag skapade mappen i fel mapp först!
 		//ska anropa modelen och få tilbaka svar som i sin tur skickas till view!
 
-		public $fullUserName = "admin";
-		public $fullPassword = "password";
+		private $usernameInput;
+		private $userPasswordInput;
+		private $logedinStatus;
 
-		
+		public $fullUserName = "Admin";
+		public $fullPassword = "Password";
+
+		public function getLogedinStatus(){
+
+			$this->logedinStatus = $logedinStatus;//skickar tillbaka vilken status man har om man är inlogad eller inte!
+		}
+
+		public function trylogingin($Username, $Password){
+
+			$this->usernameInput = trim($Username);// dessa två kommer ta bort alla onödiga blankspace osv. ifårn mina strängar!
+			$this->userPasswordInput = trim($Password);
+
+			if()//här ska jag implementera de olika kraven man får t.ex. inte skriva i fält som är tomma!
+			{
+
+			}
+		}
 		
 
 		
